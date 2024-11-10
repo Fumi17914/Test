7e915f5bc7032c62683bd4f512ec90c0c57f9a36
jfumanal <jfumanal@emeal.nttdata.com>
2024-11-10 22:30:58 +0100

    first commit

---
+++ Tes/Tes/TesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesApp.swift
+//  Tes
+//
+//  Created by Jose Fumanal Quintana on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
